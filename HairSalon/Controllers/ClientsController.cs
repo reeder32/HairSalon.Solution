5c66b9f4a6e9960e3b58fa9bdbfc4897d875c6f6
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-21 11:49:35 -0700

    :card_file_box: add delete functionality for client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -28,13 +28,41 @@ namespace HairSalon.Controllers
     {
       _db.Clients.Add(client);
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Details", new { id = client.ClientId });
     }
 
     public ActionResult Details(int id)
     {
       var client = _db.Clients.FirstOrDefault(c => c.ClientId == id);
       return View(client);
+    }
+    public ActionResult Edit(int id)
+    {
+      var client = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Clients, "StylistId", "Name");
+      return View(client);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var client = _db.Clients.FirstOrDefault(stylist => stylist.ClientId == id);
+      return View(client);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var client = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      _db.Clients.Remove(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
